c34b22b4c649a218192f6e72ef11c10915121265
ncastellanosort <nicolasfelipe00234@gmail.com>
2026-02-23 09:35:39 -0500

    feat: flask + docker + CI

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+from src.app import app
+
+def test_home_status_code_200_ok():
+    client = app.test_client()
+    resp = client.get("/")
+    assert resp.status_code == 200
+
+def test_home_response_hello_devops():
+    client = app.test_client()
+    resp = client.get("/")
+    assert resp.get_json() == {"message": "Hello DevOps"}
