96ef811b4fc761c98b9d1ad9a49499f58512dc45
ncastellanosort <nicolasfelipe00234@gmail.com>
2026-02-23 12:07:52 -0500

    test: new response to test flux

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return {"message": "Hello DevOps, this is a full cicle explaination (CI/CD)."}
+    return {"message": "Hello DevOps, this is a full cicle explaination (CI/CD). Now with Flux for GitOps"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
